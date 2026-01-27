8744e4cb414437ceb32bacefeb72d830ef2b5ff2
Peter Baintner <pb@dreisicht.net>
2026-01-27 15:25:56 +0100

    ..

--- zsh/variables.zsh
+++ zsh/variables.zsh
@@ -1,8 +1,8 @@
-export PATH="$PATH:$HOME/opt/homebrew/bin"
-export PATH="$PATH:$HOME/.fzf/bin/"
-export PATH="$PATH:$HOME/.local/bin/"
 export PATH="/opt/homebrew/opt/llvm/bin:$PATH"
-export PATH="$PATH:$HOME/.config/zsh/evanhahn/"
+export PATH="$PATH:$HOME/opt/homebrew/bin"
+export PATH="$PATH:$HOME/.fzf/bin"
+export PATH="$PATH:$HOME/.local/bin"
+export PATH="$PATH:$HOME/.config/zsh/evanhahn"
 export FORGIT_LOG_FZF_OPTS='--reverse'
 export EDITOR=code
 export QT_QPA_PLATFORMTHEME=qt6ct
