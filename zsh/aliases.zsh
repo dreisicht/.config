e30677c5a5cb74892724e4bb6872cfd44d045657
Peter Baintner <peter.baintner@airbus.com>
2026-03-17 18:26:19 +0100

    Added aliases

--- zsh/aliases.zsh
+++ zsh/aliases.zsh
@@ -3,11 +3,10 @@ alias l='eza --icons -a -s=modified -r'
 alias ll='eza --icons -a -l --git --git-repos-no-status'
 alias t='eza --icons -T'
 alias vim='nvim'
-alias c='clear'
 alias -- -="cd -"
 alias i=ipython
 alias uvr=uv run
 alias bz=bazel
 alias bl=blender
-alias just="task"
-alias j="task"
+alias bt=btop
+alias co=code
