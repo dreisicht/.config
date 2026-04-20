b4db64247abe0f80576ff05617b3244da6672d86
Peter Baintner <git@dreisicht.net>
2026-04-20 16:14:23 +0200

    Fixed little issues with git aliases

--- zsh/aliases.zsh
+++ zsh/aliases.zsh
@@ -26,8 +26,12 @@ beam() {
 }
 
 _beam_complete() {
-  local -a refs
-  refs=(${(f)"$(git for-each-ref --format='%(refname:short)' refs/heads refs/remotes refs/tags 2>/dev/null)"})
-  _describe 'ref' refs
+  local -a local_branches remote_branches tags
+  local_branches=(${(f)"$(git branch --format='%(refname:short)' 2>/dev/null)"})
+  remote_branches=(${(f)"$(git branch -r --format='%(refname:short)' 2>/dev/null)"})
+  tags=(${(f)"$(git tag 2>/dev/null)"})
+  _describe -t local-branches 'local branch' local_branches
+  _describe -t remote-branches 'remote branch' remote_branches
+  _describe -t tags 'tag' tags
 }
 compdef _beam_complete beam
